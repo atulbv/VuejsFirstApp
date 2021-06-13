84c036b8162741a88a6b2158046d55d731383044
Atul <vernekaratul@gmail.com>
2021-06-13 16:27:24 +0530

    Added Routes,data binding

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <av></av>
+    <app-header></app-header>
+    <router-view></router-view>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import AddBlogVue from "./components/AddBlog.vue";
+import FooterVue from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+import IndexVue from "./components/Index.vue";
+
 export default {
+  components: {
+    "app-header": Header,
+    "app-footer": FooterVue,
+    "app-AddBlog": AddBlogVue,
+    "app-Main": IndexVue
+  },
   data() {
-    return {
-      title: "Atul App"
-    };
+    return {};
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
-import Av from "./Atul.vue";
+import Routes from "./routes";
+Vue.use(VueRouter);
 
-Vue.component("av", Av);
+const router = new VueRouter({
+  routes: Routes,
+  mode: "history"
+});
+// import Av from "./Atul.vue";
+
+// Vue.component("av", Av);
 
 new Vue({
   el: "#app",
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 });
